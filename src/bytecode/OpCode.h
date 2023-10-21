35458e0f93702b316990009f769e48ccfd3f614d
hlx <1198816371@qq.com>
2023-10-21 08:22:09 +0000

    初始化提交

---
+++ src/bytecode/OpCode.h
@@ -0,0 +1,6 @@
+#ifndef __OpCode_h
+#define __OpCode_h
+
+#define OP_HALT 0x00
+
+#endif
---
+++ src/vm/EvaVM.h
@@ -0,0 +1,41 @@
+#ifndef __EvaVM_h
+#define __EvaVM_h
+
+#include <string>
+#include <vector>
+#include "../bytecode/OpCode.h"
+
+#define READ_BYTE() *ip++
+
+class EvaVM
+{
+public:
+    EvaVM()
+    {
+    }
+
+    void exec(const std::string &program)
+    {
+        code = {OP_HALT};
+        ip = &code[0];
+        return eval();
+    }
+
+    void eval()
+    {
+        for (;;)
+        {
+            switch (READ_BYTE())
+            {
+            case OP_HALT:
+                return;
+            }
+        }
+    }
+
+    uint8_t *ip;
+
+    std::vector<uint8_t> code;
+};
+
+#endif
